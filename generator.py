9cb599aa12e5153a432c13966aa6e35c52f449b7
Peter Idoko <pidoko@hotmail.com>
2023-11-17 15:47:57 -0800

    change get_input() to create_password()

--- generator.py
+++ generator.py
@@ -6,33 +6,29 @@ from string import digits, ascii_letters, punctuation
 # original alphanumeric string with punctuations
 ALL_LETTERS_DIGITS = digits + ascii_letters + punctuation
 
-def get_input() -> int:
+def create_password() -> str:
     '''
-    Checks that user input is between 10 and 20 inclusive
+    Checks that password length is greater than 9
     Prints error messages and runs until an accurate number is entered
     '''
     try:
-        user_input = int(input("Enter password length between 10 to 20: "))
-        while user_input < 10 :
-            if user_input < 10:
-                print("Enter a value greater than 10.")
-            else:
-                print("Enter an integer between 10 and 20.")
-            user_input = int(input("Length of password to be created (10 - 20): "))
+        user_input = int(input("Enter a password length greater than or equal to 10: "))
+        while user_input < 10 :  # NIST SP 800-132 standard
+            print("Password length too short.")
+            user_input = int(input("Enter a password length greater than or equal to 10: "))
         else:
             # random password generation
             answer = ''.join(random.choices(ALL_LETTERS_DIGITS, k = user_input))
-            print(answer)
             return answer
     except ValueError:  # if int cannot cast string
         print("Enter a number between 10 and 20")
     except:  # catch all other errors
         print("Something went wrong")
-    get_input()
+    create_password()
     
 def main():
     print("Welcome to the Password Generator.")
-    return get_input()
+    return create_password()
 
 if __name__ == "__main__":
     main()
