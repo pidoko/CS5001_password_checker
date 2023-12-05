54d4ffcc399977768b8ff085ee70b16cd8557dc2
peter.idoko <pidoko@hotmail.com>
2023-12-05 15:11:12 -0800

    entropy and time functions added and main updated

--- driver.py
+++ driver.py
@@ -10,12 +10,15 @@ from generator import (
     type_password,
 )
 
-def calculator(ALL_LETTERS_DIGITS: str, output: str) -> tuple:
+def entropy(ALL_LETTERS_DIGITS: str, output: str) -> float:
     # Measure of how hard it is to use brute-force to guess the password.
-    entropy = round(math.log2(len(ALL_LETTERS_DIGITS) ** len(output)), 2)
+    answer = round(math.log2(len(ALL_LETTERS_DIGITS) ** len(output)), 2)
+    return answer
+
+def time(ALL_LETTERS_DIGITS: str, output: str) -> float:
     # Estimate at how long it takes to crack the password using a PC with a 3.5GHz processor
-    time = round(((len(ALL_LETTERS_DIGITS) ** len(output)) / (3.154 * (10 ** 8) * (10 ** 9))), 0)  # years to crack based on 1billion passwords per second
-    return (entropy, time)
+    answer = round(((len(ALL_LETTERS_DIGITS) ** len(output)) / (3.154 * (10 ** 8) * (10 ** 9))), 0)  # years to crack based on 1billion passwords per second
+    return answer
 
 def main():
     print("Welcome to the Password Generator.")
@@ -27,9 +30,11 @@ def main():
     else:
         user_input = int(input("How long do you want your password to be? minimum 12, max 20: "))
         output = create_password(user_input)
+    
+    entropy = entropy(ALL_LETTERS_DIGITS, output)
+    time = time(ALL_LETTERS_DIGITS, output)
 
     return print(f"Your password is {output}\nIt has an entropy of {entropy}.\nIt will take about {time} years to crack using brute-force.")
-
 
 if __name__ == "__main__":
     main()
